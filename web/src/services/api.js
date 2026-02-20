85f89f2601e5d037fc8d8ba20b84460afba4a689
Khushi Bhati <khushibhati2413@gmail.com>
2026-02-20 17:57:41 +0530

    Change API base URL to production endpoint

--- web/src/services/api.js
+++ web/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE = 'http://localhost:5000';
+const API_BASE = 'https://micro-marketplace-app-h4xd.onrender.com/';
 
 const api = axios.create({
     baseURL: API_BASE,
