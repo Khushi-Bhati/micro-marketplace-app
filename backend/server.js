082609243f388b2bc2f4b6d887885b2314b4a122
Khushi Bhati <khushibhati2413@gmail.com>
2026-02-20 17:19:49 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -11,9 +11,14 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(
+  cors({
+    origin: "https://micro-marketplace-app-xi.vercel.app",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true
+  })
+);
+
 
 // Static files for uploaded images
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
